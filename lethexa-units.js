e7e6fb3e5cb784a4302e99c7a77914380aa611ee
Tim Leerhoff <tleerhof@web.de>
2016-02-27 16:04:10 +0100

    Updated to version 0.0.6

---
+++ lethexa-units.js
@@ -0,0 +1,110 @@
+/* global exports */
+
+(function(exports) {
+  'use strict';
+
+  exports.GRAVITY = 9.81;
+  exports.LIGHTSPEED = 299792458;
+  exports.SNDSPEED_WATER = 1400.0;
+  exports.SNDSPEED_AIR = 333.0;
+  exports.BOLTZMANN_CONST = 1.380650524e-23;
+  exports.MIN_TEMP_DEG = -273.15;
+  exports.METERS_PER_FOOT = 0.3048;
+  exports.METERS_PER_NAUTMILE = 1852.0;
+  exports.HOUR_TO_SECS = 3600.0;
+  exports.ASTRONOMICAL_UNIT = 149597870691.0; // [m]
+
+
+  /**
+   * Converter class for converting to and from SI-units.
+   * @class Converter
+   * @param factorToSI {Number} The conversion factor to SI-units.
+   * @param unitNameSI {String} The name of the unit in SI system (e.g. meters).
+   * @param unitName {String} The name of the unit in non-SI system (e.g. feet).
+   */
+  exports.Converter = function(factorToSI, unitNameSI, unitName) {
+    var self = this;
+
+    /**
+     * Converts the given value from other units to SI-units.
+     * @method toSI
+     * @param value {Number} The value to convert.
+     * @return The value in SI-units.
+     */
+    this.toSI = function(value) {
+      return value * factorToSI;
+    };
+
+    /**
+     * Converts the given value from SI-units to other units.
+     * @method fromSI
+     * @param value {Number} The value to convert.
+     * @return The value in non-SI-units.
+     */
+    this.fromSI = function(value) {
+      return value / factorToSI;
+    };
+
+    /**
+     * Returns the units name in SI system.
+     * @method getUnitNameSI
+     * @return The units name in SI system.
+     */
+    this.getUnitNameSI = function() {
+      return unitNameSI;
+    };
+
+    /**
+     * Returns the units name in non-SI system.
+     * @method getUnitName
+     * @return The units name in non-SI system.
+     */
+    this.getUnitName = function() {
+      return unitName; 
+    };
+
+    /**
+     * Returns the given value as SI-unit-string e.g. '10m'.
+     * @method asStringSI
+     * @return The given value as string.
+     */
+    this.asStringSI = function(value) {
+      return '' + self.toSI(value) + unitNameSI;
+    };
+
+    /**
+     * Returns the given value as non-SI-unit-string e.g. '10ft'.
+     * @method asString
+     * @return The given value as string.
+     */
+    this.asString = function(value) {
+      return '' + self.fromSI(value) + unitName;
+    };
+  };
+
+  exports.NONE = new exports.Converter(1.0, '', '');
+  exports.PERCENT = new exports.Converter(0.01, '', '%');
+  exports.LATITUDE = new exports.Converter(1.0, '°', '°');
+  exports.LONGITUDE = new exports.Converter(1.0, '°', '°');
+  exports.AREA = new exports.Converter(1.0, 'm²', 'm²');
+  exports.ACCEL = new exports.Converter(1.0, 'm/s²', 'm/s²');
+  exports.SPEED = new exports.Converter(1.0, 'm/s', 'm/s');
+  exports.DISTANCE = new exports.Converter(1.0, 'm', 'm');
+  exports.ROTACCEL = new exports.Converter(1.0, 'rad/s²', 'rad/s²');
+  exports.ROTSPEED = new exports.Converter(1.0, 'rad/s', 'rad/s');
+  exports.DIRECTION = new exports.Converter(Math.PI / 180.0, 'rad', '°');
+  exports.PRESSURE = new exports.Converter(1.0, 'hPa', 'hPa');
+  exports.ALTITUDE = new exports.Converter(1.0, 'm', 'm');
+  exports.DIAMETER = new exports.Converter(1.0, 'm', 'm');
+  exports.RADIUS = new exports.Converter(1.0, 'm', 'm');
+  exports.LENGTH = new exports.Converter(1.0, 'm', 'm');
+  exports.FREQUENCY = new exports.Converter(1.0, 'Hz', 'Hz');
+  exports.POWER = new exports.Converter(1.0, 'W', 'W');
+  exports.WEIGHT = new exports.Converter(1.0, 'kg', 'kg');
+  exports.RELTIME = new exports.Converter(1.0, 's', 's');
+  exports.ABSTIME = new exports.Converter(1.0, 's', 's');
+
+
+})(typeof exports === 'undefined'? this.units={}: exports);
+
+
